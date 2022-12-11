fae91b8788a9f9c4875bf6fb6c76a316f002f09f
sumin <dltnals5763@naver.com>
2022-12-11 16:37:41 +0900

    review: [7~11/54] 자바 ORM 표준 JPA 프로그래밍 - 기본편

--- src/main/java/hellojpa/JpaMain.java
+++ src/main/java/hellojpa/JpaMain.java
@@ -4,7 +4,6 @@ import javax.persistence.EntityManager;
 import javax.persistence.EntityManagerFactory;
 import javax.persistence.EntityTransaction;
 import javax.persistence.Persistence;
-import java.util.List;
 
 public class JpaMain {
     public static void main(String[] args) {
@@ -16,15 +15,11 @@ public class JpaMain {
         ts.begin();
 
         try {
-//            Member findMember = em.find(Member.class, 1L);
-//            findMember.setName("helloJPA");
-            List<Member> result = em.createQuery("select m from Member m", Member.class)
-                    .setFirstResult(1)
-                    .setMaxResults(10)
-                    .getResultList();
-            for (Member member : result) {
-                System.out.println("member.name= " + member.getName());
-            }
+            Member member = new Member(200L, "member200");
+            em.persist(member);
+
+            em.flush();
+
             ts.commit();
         } catch (Exception e) {
             ts.rollback();
